9e4050151ebe0cbfb48eea8d764d459ca510d3d7
093-Sahil-kr <144386688+093-Sahil-kr@users.noreply.github.com>
2023-09-27 21:06:59 +0530

    Add files via upload

---
+++ src/mongodb.js
@@ -0,0 +1,27 @@
+const mongoose=require("mongoose")
+
+mongoose.connect("mongodb://127.0.0.1:27017/LoginSignup")
+.then(()=>{
+    console.log("mongodb connected");
+})
+.catch(()=>{
+    console.log("failed to connect");
+})
+
+const LogInSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        require:true
+    },
+    email:{
+        type:String,
+        require:true
+    },
+    password:{
+        type:String,
+        require:true
+    }
+})
+
+const collection=new mongoose.model("Collection1",LogInSchema)
+module.exports=collection
